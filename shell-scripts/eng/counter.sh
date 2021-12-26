15fe91757646ffaf44b745d578e1f584c78cb088
Burak Seçer <buraksecer@users.noreply.github.com>
2021-12-26 16:40:46 +0300

    Create counter.sh

---
+++ shell-scripts/eng/counter.sh
@@ -0,0 +1,10 @@
+echo "Starting counter..."
+
+for i in 1 2 3 4 5 6 7 8 9 10
+do
+   echo "$i"
+done
+
+echo "Done counter..."
+
+echo "Processes are done!" >> /home/parallels/Desktop/run-report.log
